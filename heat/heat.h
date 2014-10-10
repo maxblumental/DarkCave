0df61cc888cd44c01600f6c862641a35cea9a28c
Maxim Blumental <bvmaks@gmail.com>
2014-10-10 14:51:25 +0400

    [MPI] Fixed bugs with memory allocation. Corrected coding style.

--- heat/heat.h
+++ heat/heat.h
@@ -2,10 +2,10 @@
 
 #include <math.h>
 #include <mpi.h>
-#define TASK(n,m,k) (((n)-(k)) % (m) == 0)?\
+#define TASK(n,m,k) (((n)-(k)) % (m)==0)?\
                     (((n)-(k))/(m)):\
                     (((n)-(k))/(m) + 1)
-int estimateNtau(int T,//total time
+int estimateNtau(int T,/*total time*/
                  int N/*num of intervals on an axis*/) {
   return 10*T*N*N;
 }
@@ -27,9 +27,9 @@ void printTemperatureNet(double** u, int nx, int ny) {
 
 double** allocNet(int nx, int ny) {
     int i=0;
-    double** array = (double**)malloc(sizeof(double*)*nx);
+    double** array=(double**)malloc(sizeof(double*)*nx);
     for(i=0;i<nx;i++) {
-      array[i] = (double*)malloc(sizeof(double)*ny);
+      array[i]=(double*)malloc(sizeof(double)*ny);
     }
     return array;
 }
@@ -39,7 +39,7 @@ void initNet(int N, int nx, int offset,
   int i=0, j=0;
   for (i=0;i<nx;i++) {
     for (j=0;j<N+1;j++) {
-      array[i][j] = uzero(a, b, ((double)(i+offset))/N, ((double)j)/N);
+      array[i][j]=uzero(a, b, ((double)(i+offset))/N, ((double)j)/N);
     }
   }
 }
@@ -53,44 +53,37 @@ double calcNode(double tau, int N,
            -4*array[i][j]);
 }
 
-double** solveHeatEquation(int N, //num of intervals on an axis
-                           double T, //total time
-                           double a, double b,//coefficients for u(x,y,0)
+double** solveHeatEquation(int N,/*num of intervals on an axis*/
+                           double T,/*total time*/
+                           double a, double b,/*coefficients for u(x,y,0)*/
                            int Ntau/*num of intervals on time axis*/) {
-  int size = 0, rank = 0;
+  int size=0, rank=0;
 
   MPI_Comm_size( MPI_COMM_WORLD, &size);
   MPI_Comm_rank( MPI_COMM_WORLD, &rank);
   
-  //Determine elementary task
-  int task = TASK(N-1, size, rank);
+  int task=TASK(N-1, size, rank);
   
-  //malloc
+  //memory allocation
   double** net0, **net;
-  int i = 0, j=0;
-  double tau = T/(double)Ntau;
+  int i=0, j=0;
+  double tau=T/(double)Ntau;
   if (rank==0) {
-      net0 = allocNet(N+1, N+1);    
+      net0=allocNet(N+1, N+1);    
       initNet(N, N+1, 0, a, b, net0);
-      net = allocNet(N+1, N+1);    
-//      fprintf(stderr, "==>after init\n");
-//      printTemperatureNet(net0, N+1, N+1);
+      net=allocNet(N+1, N+1);    
   } else if (rank==size-1) {
-      net0 = allocNet(task+2, N+1);    
+      net0=allocNet(task+2, N+1);    
       initNet(N, task+2, N-task-1, a, b, net0);
-      net = allocNet(task+2, N+1);    
-//      fprintf(stderr, "-->[%d]after init\n", rank);
-//      printTemperatureNet(net0, task+2, N+1);
+      net=allocNet(task+2, N+1);    
   } else {
-      net0 = allocNet(task+2, N+1);    
-      net = allocNet(task+2, N+1);    
+      net0=allocNet(task+2, N+1);    
+      net=allocNet(task+2, N+1);    
      
-      int offset = 0;
+      int offset=0;
       for (i=0;i<rank;i++)
-        offset += TASK(N-1, size, i);
+        offset+=TASK(N-1, size, i);
       initNet(N, task+2, offset, a, b, net0);
-//      fprintf(stderr, "-->[%d]after init\n", rank);
-//      printTemperatureNet(net0, task+2, N+1);
   }
 
   
@@ -101,26 +94,24 @@ double** solveHeatEquation(int N, //num of intervals on an axis
   for (k=0;k<Ntau+1;k++) {
     for (i=1;i<task+1;i++) {
      for (j=1;j<N;j++) {
-        net[i][j] = calcNode(tau, N, i, j, net0);
+        net[i][j]=calcNode(tau, N, i, j, net0);
       }
     }
     MPI_Barrier(MPI_COMM_WORLD);
     for (i=1;i<task+1;i++) {
       for (j=1;j<N;j++) {
-        net0[i][j] = net[i][j];
+        net0[i][j]=net[i][j];
       }
     }
     //exchange
     if (rank==0) {
-
-    //exchange
       if (size>1) {
         MPI_Recv((void*)(net0[task+1]+1), N-1, MPI_DOUBLE, 1,
                  0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         MPI_Send((void*)(net0[task]+1), N-1, MPI_DOUBLE, 1, 0, MPI_COMM_WORLD);
       }
-    } else if (rank == size-1) {
-      if (rank%2 == 1) {
+    } else if (rank==size-1) {
+      if (rank%2==1) {
         MPI_Send((void*)(net0[0]+1), N-1, MPI_DOUBLE, rank-1, 0, MPI_COMM_WORLD);
         MPI_Recv((void*)(net0[0]+1), N-1, MPI_DOUBLE, rank-1,
                  0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
@@ -129,7 +120,7 @@ double** solveHeatEquation(int N, //num of intervals on an axis
                  0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         MPI_Send((void*)(net0[0]+1), N-1, MPI_DOUBLE, rank-1, 0, MPI_COMM_WORLD);
       }
-    } else if (rank%2 == 1) {
+    } else if (rank%2==1) {
       MPI_Send((void*)(net0[0]+1), N-1, MPI_DOUBLE, rank-1, 0, MPI_COMM_WORLD);
       MPI_Recv((void*)(net0[0]+1), N-1, MPI_DOUBLE, rank-1,
                0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
@@ -143,9 +134,8 @@ double** solveHeatEquation(int N, //num of intervals on an axis
       MPI_Recv((void*)(net0[0]+1), N-1, MPI_DOUBLE, rank-1,
                0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
       MPI_Send((void*)(net0[0]+1), N-1, MPI_DOUBLE, rank-1, 0, MPI_COMM_WORLD);
-    } //exchange
-  } // layer-loop
-//  fprintf(stderr, "-->[%d]calculation/exchange is over\n", rank);
+    }//exchange
+  }//layer-loop
 
   //gather
   if (rank>0) {
@@ -154,47 +144,35 @@ double** solveHeatEquation(int N, //num of intervals on an axis
   }
   
   if (rank==0) {
-    int disp = 0;
-    int taski = 0;
+    int disp=0;
+    int taski=0;
     for (i=1;i<size;i++) {
-      taski = TASK(N-1, size, i);
-//      fprintf(stderr, "==>recv for proc[%d]\n", i);
+      taski=TASK(N-1, size, i);
       for (j=0;j<taski;j++){
-//        fprintf(stderr, "==>column %d from proc[%d]\n", j, i);
-//        fprintf(stderr, "task+1:%d, disp:%d, j:%d\n", task+1,disp,j);
         MPI_Recv((void*)net0[task+1+disp+j], N-1, MPI_DOUBLE, i,
                  0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
       }
-      disp += TASK(N-1, size, i);
+      disp+=TASK(N-1, size, i);
     }
   }
-//  fprintf(stderr, "-->[%d]gather is over\n", rank);
 
-  MPI_Barrier(MPI_COMM_WORLD);
- 
   if (rank>0) {
     for (i=0;i<task+2;i++) {
-//      fprintf(stderr, "-->[%d]column %d freed\n", rank, i);
       free(net0[i]);
       free(net[i]);
     }
-//    fprintf(stderr, "-->[%d]nets freed\n", rank);
     free(net0);
     free(net);
-    fprintf(stderr, "-->[%d]free is over\n", rank);
   }
   
   
   if (rank==0) {
     for (i=0;i<N+1;i++) {
-//      fprintf(stderr, "==>column %d freed\n", i);
       free(net[i]);
     }
-//    fprintf(stderr, "==>net freed\n");
     free(net);
-//    fprintf(stderr, "==>free is over\n");
     return net0;
   }
-  fprintf(stderr, "-->[%d]NULL is returned\n", rank);
+  
   return NULL;
 }
--- heat/main.c
+++ heat/main.c
@@ -11,44 +11,39 @@ int main(int argc, char** argv) {
 
   MPI_Init(NULL,NULL);
   
-  int size = 0, rank = 0;
+  int size=0, rank=0;
 
   MPI_Comm_size( MPI_COMM_WORLD, &size);
   MPI_Comm_rank( MPI_COMM_WORLD, &rank);
 
-  /*Get the args:
-   *OUT: T N a b*/
-  double T = atof(argv[1]),
-         a = atof(argv[3]),
-         b = atof(argv[4]);
-  int N = atoi(argv[2]);
-  /*Estimate tau:
-   *OUT: tau*/
-  int Ntau = estimateNtau(T, N);
-  fprintf(stderr, "{main}-->[%d]BEGIN\n", rank);
-  /*Get num of processes:
-   *OUT: n*/
-
-  /*Build the system and solve it:
-   *IMPL: loop T/tau, (N+1)(N+1) arrays
-   *INPUT: T N a b tau
-   *OUT: double** u[N+1][N+1] for t = T*/
-  double** solution = solveHeatEquation(N, T, a, b, Ntau);
-  fprintf(stderr, "{main}-->[%d]solved\n", rank);
-  
-  /*Output:
-   *IMPL: array printer.*/
+  double T=atof(argv[1]),
+         a=atof(argv[3]),
+         b=atof(argv[4]);
+  int N=atoi(argv[2]);
  
-  if (rank == 0) {
-    fprintf(stderr, "{main}==>right before print\n");
+  int Ntau=estimateNtau(T, N);
+
+  double start=0, end=0;
+  MPI_Barrier(MPI_COMM_WORLD);
+  if (rank==0)
+    start=MPI_Wtime();
+
+  /*Build the system and solve it*/
+  double** solution=solveHeatEquation(N, T, a, b, Ntau);
+  
+  if (rank==0) {
     printTemperatureNet(solution, N+1, N+1);
     int i=0;
     for (i=0;i<N+1;i++) {
-//      fprintf(stderr, "{main}==>column %d freed\n", i);
       free(solution[i]);
     }
-    fprintf(stderr, "{main}==>net freed\n");
     free(solution);
+  }
+
+  MPI_Barrier(MPI_COMM_WORLD);
+  if (rank==0) {
+    end=MPI_Wtime();
+    printf("time=%lf sec\n", end-start);
   }
 
   MPI_Finalize();
